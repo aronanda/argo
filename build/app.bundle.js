f5f2938dc89d912a7000e3660db9f32d2f959d22
icebox <albertosantini@gmail.com>
2017-05-28 00:16:33 +0200

    chore(bundle): update

--- build/app.bundle.js
+++ build/app.bundle.js
@@ -2168,7 +2168,7 @@ class ToastsTemplate {
         render`
             <table class="f6 ba" cellspacing="0">
                 <tbody>${
-                    state.toasts.map(toast => hyperHTML.wire(toast, ":tr")`<tr>
+                    state.toasts.map(toast => `<tr>
                         <td class="b--black-20 pr2"> ${Util.getHHMMSSfromDate(toast.date)} </td>
                         <td class="b--black-20 pl2"> ${toast.message} </td>
                     </tr>`
