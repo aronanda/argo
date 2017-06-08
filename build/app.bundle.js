ef186b0147b044409857e3382fa6b74c942ddc91
icebox <albertosantini@gmail.com>
2017-06-08 13:25:34 +0200

    fix(quotes): fix quotes list order

--- build/app.bundle.js
+++ build/app.bundle.js
@@ -591,39 +591,27 @@ class QuotesService {
         const account = AccountsService.getAccount(),
             streamingInstruments = account.streamingInstruments,
             pips = account.pips,
-            instrument = tick.instrument;
+            instrument = tick.instrument,
+            lenStreamingInstruments = Object.keys(streamingInstruments).length,
+            lenQuotesInstruments = Object.keys(QuotesService.quotes).length;
 
-        QuotesService.quotes[instrument] = {
-            time: tick.time,
-            ask: tick.ask,
-            bid: tick.bid,
-            spread: ((tick.ask - tick.bid) / pips[instrument]).toFixed(1)
-        };
-
-
-        const accountInstruments = JSON.stringify(streamingInstruments);
-        const ticksInstruments = JSON.stringify(
-            Object.keys(QuotesService.quotes));
-
-        if (accountInstruments !== ticksInstruments) {
+        if (lenStreamingInstruments !== lenQuotesInstruments) {
             streamingInstruments.forEach(instr => {
-                let temp;
-
-                if (JSON.stringify(QuotesService.quotes[instr]) !== "{}") {
-                    temp = QuotesService.quotes[instr];
-                    QuotesService.quotes[instr] = {};
-                    QuotesService.quotes[instr] = temp;
-                }
+                QuotesService.quotes[instr].instrument = instr;
             });
         }
+
+        QuotesService.quotes[instrument].time = tick.time;
+        QuotesService.quotes[instrument].ask = tick.ask;
+        QuotesService.quotes[instrument].bid = tick.bid;
+        QuotesService.quotes[instrument].spread =
+            ((tick.ask - tick.bid) / pips[instrument]).toFixed(1);
     }
 
     static reset() {
-        let key;
-
-        for (key in QuotesService.quotes) {
-            if (QuotesService.quotes.hasOwnProperty(key)) {
-                delete QuotesService.quotes[key];
+        for (const instr in QuotesService.quotes) {
+            if (QuotesService.quotes[instr].instrument === instr) {
+                delete QuotesService.quotes[instr];
             }
         }
     }
